cfce0e6e31f35823e790120ceed07dc91f79275c
marcogaro <68847387+marcogaro@users.noreply.github.com>
2020-09-01 11:02:16 +0200

    Update restart.sh

--- permessifunzionante01092020/restart.sh
+++ permessifunzionante01092020/restart.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
 
+#if [ "$#" -ne 1 ]; then
+  #  echo "Usage: destroy_virtual_rasp.sh <virtual_rasp_name>"
+ #   exit;
+#fi
+
+#name=$1
+
+
+#echo nome della raspberry?
+#read name
+
+file=passt2.4
+
+
+echo "qual è il nome della raspberry per cui riavviare il filesystem? "
+read -r nome
+
+
+pass=""$file"."$nome".py"
+echo $pass
+
+
+
+echo "restarting filesystem "$nome"!"
+
+sudo umount --force /gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio
+sudo umount --force /gpio_mnt/"$nome"/sys/class/gpio
+
+
+python3 "$pass" /sys/devices/platform/soc/3f200000.gpio /gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio/ &
+python3 "$pass" /sys/class/gpio/ /gpio_mnt/"$nome"/sys/class/gpio/ &
