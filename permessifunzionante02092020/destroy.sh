c80bbdeff360750f3112c22417a883bd8f2f4bd3
marcogaro <68847387+marcogaro@users.noreply.github.com>
2020-09-02 17:39:31 +0200

    Update destroy.sh

--- permessifunzionante02092020/destroy.sh
+++ permessifunzionante02092020/destroy.sh
@@ -22,11 +22,14 @@ read -r nome
 
 echo "Destroying virtual rasp "$nome"!"
 
-sudo rm -rf $pass.$nome.py
+
 
 
 sudo umount /gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio
 sudo umount /gpio_mnt/"$nome"/sys/class/gpio
+
+sudo rm -rf $pass.$nome.py
+
 sudo umount /gpio_mnt/sys/devices/platform/soc/soc\:firmware/soc\:firmware\:expgpio/gpio/gpiochip504/
 sudo umount --force /gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio
 sudo umount --force /gpio_mnt/"$nome"/sys/class/gpio
